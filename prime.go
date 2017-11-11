12f7950e94e6e4852fb95e1099bd9ead3f0fb0bb
calllivecn <c-all@qq.com>
2017-11-11 17:24:47 +0800

    修改：prime.go 修改：prime.py

--- prime.go
+++ prime.go
@@ -5,13 +5,13 @@ import (
 	"math"
 	)
 
-var NUMBER uint64 = 10000000
+var NUMBER uint64 = 1000
 
 func main(){
 	var i uint64
 	for i=1;i <= NUMBER;i++{
 	if prime(i) {
-		//fmt.Println(i,"是素数")
+		fmt.Println(i,"是素数")
 		}
 		}
 fmt.Println("run done Go lang")
@@ -25,7 +25,7 @@ func prime(n uint64)bool{
 		return true
 		}
 
-	for i=2;i < uint64(math.Sqrt(float64(n)));i++ {
+	for i=2;i < uint64(math.Sqrt(float64(n)));i+=2 {
 		if 0 == n % i {
 			return false
 			}
--- prime.py
+++ prime.py
@@ -6,23 +6,23 @@ import math
 
 
 def prime(n):
-	if n <= 2 :
-		return True
-	
-	for i in range(2,int(math.sqrt(n))):
-		if 0 == n % i:
-			return False
+    if n <= 2 :
+        return True
+    
+    for i in range(2,int(math.sqrt(n))):
+        if 0 == n % i:
+            return False
 
-	return True
+    return True
 
 def main(m):
-	for i in range(1,m+1,2):
-		if prime(i):
-			pass
-			#print(i,"是素数")
+    for i in range(1,m+1,2):
+        if prime(i):
+            pass
+            #print(i,"是素数")
 
 
 
 if __name__ == "__main__":
-	main(10000000)
+    main(1000)
 
