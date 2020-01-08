a47b6facf7a1d8e2d5bb07d7c64a9a5b019eadb4
calllivecn <calllivecn@outlook.com>
2020-01-08 09:18:15 +0800

    修改：     errors.go 修改：     select.go 新文件：   ticker.go

--- errors.go
+++ errors.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    )
+	"fmt"
+	"time"
+)
 
-type MyError struct{
-    When time.Time
-    What string
+type MyError struct {
+	When time.Time
+	What string
 }
 
-
-func (e *MyError) Error() string{
-    return fmt.Sprintf("at %v, %s",e.When,e.What)
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
 }
 
 func run() error {
-return &MyError{time.Now(),"it didnt work"}
+	return &MyError{time.Now(), "it didnt work"}
 }
 
-func main(){
+func main() {
 
-    if err:=run(); err!=nil{
-        fmt.Println(err)
-    }else{
-        fmt.Println("it's work")
-    }
+	if err := run(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("it's work")
+	}
 }
---
+++ ticker.go
@@ -0,0 +1,26 @@
+/*
+# date 2020-01-06 10:15:11
+# author calllivecn <c-all@qq.com>
+*/
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main(){
+
+    ticker := time.NewTicker(time.Millisecond * 500)
+
+    go func() {
+        for t := range ticker.C {
+            fmt.Println("Tick at ", t)
+        }
+    }()
+
+    time.Sleep(time.Second * 5)
+    ticker.Stop()
+    fmt.Println("ticker stop")
+}
